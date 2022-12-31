32c10ff4c3ce163978ada01c80bed13318dc57fd
emmanuel <gbengacode>
2022-12-31 05:49:50 +0100

    remove bugs

--- controllers/movie.controller.js
+++ controllers/movie.controller.js
@@ -1,61 +1,67 @@
 import Movies from "../model/movie.model.js";
-import movieData from "../data/movie.json" assert { type: "json" };
 
 const movieController = {};
-const genreOptions = [
-  "Action",
-  "Romance",
-  "Fantasy",
-  "Drama",
-  "Crime",
-  "Adventure",
-  "Thriller",
-  "Sci-fi",
-  "Music",
-  "Family",
-];
+
 movieController.getMovies = async (req, res) => {
-  let {
-    page = 0,
-    limit = 5,
-    search = " ",
-    sort = "rating",
-    genre = "all",
-  } = req.query;
-  try {
-    genre === "all" ? (genre = [...genreOptions]) : genre.split(",");
-    sort ? (sort = sort.split(",")) : (sort = [sort]);
+
+   try {
+    const page = parseInt(req.query.page) - 1 || 0;
+    const limit = parseInt(req.query.limit) || 5;
+    const search = req.query.search || "";
+    let sort = req.query.sort || "rating";
+    let genre = req.query.genre || "All";
+  
+    const genreOptions = [
+      "Action",
+      "Romance",
+      "Fantasy",
+      "Drama",
+      "Crime",
+      "Adventure",
+      "Thriller",
+      "Sci-fi",
+      "Music",
+      "Family",
+    ];
+  
+    genre === "All"
+      ? (genre = [...genreOptions])
+      : (genre = req.query.genre.split(","));
+    req.query.sort ? (sort = req.query.sort.split(",")) : (sort = [sort]);
+  
     let sortBy = {};
     if (sort[1]) {
       sortBy[sort[0]] = sort[1];
     } else {
       sortBy[sort[0]] = "asc";
     }
-    const movies = await Movies.find()
+  
+    const movies = await Movies.find({ name: { $regex: search, $options: "i" } })
       .where("genre")
       .in([...genre])
       .sort(sortBy)
       .skip(page * limit)
       .limit(limit);
-    const total = Movies.countDocuments({
+  
+    const total = await Movies.countDocuments({
       genre: { $in: [...genre] },
       name: { $regex: search, $options: "i" },
     });
-    return res.status(200).json({ success: true, movies });
-  } catch (error) {
-
+  
+    const response = {
+      success: true,
+      total,
+      page: page + 1,
+      limit,
+      genres: genreOptions,
+      movies,
+    };
+  
+    res.status(200).json(response);
+   } catch(error) {
     return res.status(500).json({ success: false, error });
-  }
+   }
 };
 
-movieController.postMovie = async (req, res) => {
-  try {
-    await Movies.insertMany(movieData);
-    
-    return res.status(200).json({ success: true, message: "inserted" });
-  } catch (error) {
-    return res.status(400).json({ success: false, error });
-  }
-};
 
 export default movieController;
--- routes/movies.route.js
+++ routes/movies.route.js
@@ -3,6 +3,5 @@ import movieController from "../controllers/movie.controller.js";
 const movieRouter = express.Router();
 
 movieRouter.get("/movies", movieController.getMovies);
-movieRouter.post("/movies", movieController.postMovie);
 
 export default movieRouter;
